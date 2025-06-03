827c0b2552ed2084213976db3a1738a41207e288
careway <ctjimenez24@gmail.com>
2025-06-03 14:21:01 +0200

    Added includes and ommited changed behaviour to fit c++17 std

--- include/cwparser/ctm_optional.hpp
+++ include/cwparser/ctm_optional.hpp
@@ -1,155 +1,191 @@
-#pragma once
+#ifndef CTM_OPTIONAL_CWPARSER
+#define CTM_OPTIONAL_CWPARSER
 
 
 #if __cplusplus >= 201703L
 
 #include <optional>
 
-#else 
-#include <utility>
-#include <stdexcept>
+#else
+
+#	include <stdexcept>
+#	include <utility>
 namespace std
 {
-    template<typename T>
-    class optional {
-    public:
-        optional() : has_value_(false) {}
-    
-        optional(const T& value) : has_value_(true), value_(value) {}
-    
-        optional(T&& value) : has_value_(true), value_(std::move(value)) {}
-    
-        optional(const optional& other) {
-            if (other.has_value_) {
-                has_value_ = true;
-                value_ = other.value_;
-            } else {
-                has_value_ = false;
-            }
-        }
-    
-        optional(optional&& other) noexcept {
-            if (other.has_value_) {
-                has_value_ = true;
-                value_ = std::move(other.value_);
-                other.has_value_ = false;
-            } else {
-                has_value_ = false;
-            }
-        }
-    
-        T& operator*()
-        {
-            return value_;
-        }
-    
-        T* operator->()
-        {
-            return &value_;
-        }
-    
-        optional& operator=(const optional& other) {
-            if (this != &other) {
-                if (other.has_value_) {
-                    has_value_ = true;
-                    value_ = other.value_;
-                } else {
-                    has_value_ = false;
-                }
-            }
-            return *this;
-        }
-    
-        optional& operator=(optional&& other) noexcept {
-            if (this != &other) {
-                if (other.has_value_) {
-                    has_value_ = true;
-                    value_ = std::move(other.value_);
-                    other.has_value_ = false;
-                } else {
-                    has_value_ = false;
-                }
-            }
-            return *this;
-        }
+template <typename T>
+class optional
+{
+public:
+	optional()
+		: has_value_(false)
+		, value_(T{})
+	{ }
 
-        bool operator==(T&& other)
-        {
-            if(has_value_)
-                return value_ == other;
-            else
-                return false;
-        }
-        bool operator!=(T&& other)
-        {
-            if(has_value_)
-                return value_ == other;
-            else
-                return false;
-        }
-        bool operator<(T&& other)
-        {
-            if(has_value_)
-                return value_ < other;
-            else
-                return false;
-        }
-        bool operator<=(T&& other)
-        {
-            if(has_value_)
-                return value_ <= other;
-            else
-                return false;
-        }
+	optional(const T& value)
+		: has_value_(true)
+		, value_(value)
+	{ }
 
-        bool operator>(T&& other)
-        {
-            if(has_value_)
-                return value_ > other;
-            else
-                return false;
+	optional(T&& value)
+		: has_value_(true)
+		, value_(std::move(value))
+	{ }
 
-        }
-        bool operator>=(T&& other)
-        {
-            if(has_value_)
-                return value_ >= other;
-            else
-                return false;
+	optional(const optional& other)
+	{
+		if(other.has_value_)
+		{
+			has_value_ = true;
+			value_ = other.value_;
+		}
+		else
+		{
+			has_value_ = false;
+		}
+	}
 
-        }
+	optional(optional&& other) noexcept
+	{
+		if(other.has_value_)
+		{
+			has_value_ = true;
+			value_ = std::move(other.value_);
+			other.has_value_ = false;
+		}
+		else
+		{
+			has_value_ = false;
+		}
+	}
 
-        explicit operator bool() const{
-            return has_value_;
-        }
-        
-        bool has_value() const {
-            return has_value_;
-        }
-    
-        T& value() {
-            if (!has_value_) {
-                throw std::runtime_error("No value present");
-            }
-            return value_;
-        }
-    
-        const T& value() const {
-            if (!has_value_) {
-                throw std::runtime_error("No value present");
-            }
-            return value_;
-        }
-    
-        void reset() {
-            has_value_ = false;
-        }
-    
-    
-    private:
-        bool has_value_;
-        T value_;
-    };
-    
-}
+	T& operator*()
+	{
+		return value_;
+	}
+
+	T* operator->()
+	{
+		return &value_;
+	}
+
+	optional& operator=(const optional& other)
+	{
+		if(this != &other)
+		{
+			if(other.has_value_)
+			{
+				has_value_ = true;
+				value_ = other.value_;
+			}
+			else
+			{
+				has_value_ = false;
+			}
+		}
+		return *this;
+	}
+
+	optional& operator=(optional&& other) noexcept
+	{
+		if(this != &other)
+		{
+			if(other.has_value_)
+			{
+				has_value_ = true;
+				value_ = std::move(other.value_);
+				other.has_value_ = false;
+			}
+			else
+			{
+				has_value_ = false;
+			}
+		}
+		return *this;
+	}
+
+	bool operator==(T&& other)
+	{
+		if(has_value_)
+			return value_ == other;
+		else
+			return false;
+	}
+	bool operator!=(T&& other)
+	{
+		if(has_value_)
+			return value_ == other;
+		else
+			return false;
+	}
+	bool operator<(T&& other)
+	{
+		if(has_value_)
+			return value_ < other;
+		else
+			return false;
+	}
+	bool operator<=(T&& other)
+	{
+		if(has_value_)
+			return value_ <= other;
+		else
+			return false;
+	}
+
+	bool operator>(T&& other)
+	{
+		if(has_value_)
+			return value_ > other;
+		else
+			return false;
+	}
+	bool operator>=(T&& other)
+	{
+		if(has_value_)
+			return value_ >= other;
+		else
+			return false;
+	}
+
+	explicit operator bool() const
+	{
+		return has_value_;
+	}
+
+	bool has_value() const
+	{
+		return has_value_;
+	}
+
+	T& value()
+	{
+		if(!has_value_)
+		{
+			throw std::runtime_error("No value present");
+		}
+		return value_;
+	}
+
+	const T& value() const
+	{
+		if(!has_value_)
+		{
+			throw std::runtime_error("No value present");
+		}
+		return value_;
+	}
+
+	void reset()
+	{
+		has_value_ = false;
+	}
+
+private:
+	bool has_value_;
+	T value_;
+};
+
+} // namespace std
+#endif
+
 #endif
